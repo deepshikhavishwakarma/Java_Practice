103f04016b396c32c784fe1d0e3c75eedcb25158
Deepshikha Vishwakarma <76782726+deepshikhavishwakarma@users.noreply.github.com>
2024-07-23 16:24:37 +0000

    Solved Patterns Logic

---
+++ src/main/java/CodePractice/Codeday3/Pattern18.java
@@ -0,0 +1,19 @@
+package CodePractice.Codeday3;
+
+public class Pattern18 {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 5; i++) {
+      int a = i;
+      for (int j = i; j <= 5; j++) {
+        System.out.print("  ");
+      }
+      for (int j = 1; j <= i; j++) {
+        System.out.print(a-- + " ");
+      }
+
+      System.out.println();
+
+    }
+  }
+}
