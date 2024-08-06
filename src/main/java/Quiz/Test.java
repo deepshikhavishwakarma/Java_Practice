deeed12fc6bc75763f7b6d20b6d7af7f1cf3ab26
Deepshikha Vishwakarma <76782726+deepshikhavishwakarma@users.noreply.github.com>
2024-08-06 13:41:56 +0000

    solved some logical questions in Quiz and solved  selection sort algorithm

---
+++ src/main/java/Quiz/Test.java
@@ -0,0 +1,16 @@
+public class Test
+{
+      public static void main(String[] args)
+      {
+        while (true) {
+        if (false) {
+        System.out.println(false);
+        break;
+        } else {
+        System.out.println(true);
+        }
+        return;
+        }
+        System.out.println("out of the loop");
+}
+}
